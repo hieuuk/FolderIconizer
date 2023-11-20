865fb807e130015fbca8ee3a2a4586996ffcbaff
Hieu Pham <hieuuk@gmail.com>
2023-11-21 00:21:03 +0700

    Basic working function

---
+++ FolderIconUpdater.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FolderIconizer
+{
+    public class FolderIconUpdater
+    {
+        // P/Invoke to refresh the folder icon
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        static extern void SHChangeNotify(int wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+
+        public static void ChangeFolderIcon(string folderPath, string iconPath)
+        {
+            string desktopIniPath = Path.Combine(folderPath, "desktop.ini");
+
+            // Set folder and desktop.ini attributes
+            File.SetAttributes(folderPath, File.GetAttributes(folderPath) | FileAttributes.System);
+            if (File.Exists(desktopIniPath))
+            {
+                File.SetAttributes(desktopIniPath, FileAttributes.Normal);
+            }
+
+            // Write icon settings to desktop.ini
+            using (StreamWriter writer = new StreamWriter(desktopIniPath, false))
+            {
+                writer.WriteLine("[.ShellClassInfo]");
+                writer.WriteLine($"IconResource={iconPath},0");
+                writer.Flush();
+            }
+
+            // Set desktop.ini to hidden
+            File.SetAttributes(desktopIniPath, FileAttributes.Hidden | FileAttributes.System);
+
+            // Refresh the icon
+            SHChangeNotify(0x08000000, 0x0000, IntPtr.Zero, IntPtr.Zero);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace FolderIconizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // P/Invoke to refresh the folder icon
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        static extern void SHChangeNotify(int wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void WriteLog(string message)
+        {
+            tbLogs.Text = $"{DateTime.Now.ToString("HH:mm")}: {message} \n" + tbLogs.Text;
+        }
+
+        private async void cbProcess_Click(object sender, RoutedEventArgs e)
+        {
+            await UpdateFolderIcons(tbFolderPath.Text);
+        }
+
+
+        public async Task UpdateFolderIcons(string parentFolderPath)
+        {
+            // Ensure the parent folder exists
+            if (!Directory.Exists(parentFolderPath))
+            {
+                System.Windows.MessageBox.Show($"The directory '{parentFolderPath}' was not found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Iterate through each subfolder in the parent folder
+            foreach (var folderPath in Directory.GetDirectories(parentFolderPath))
+            {
+                string iconFilePath = System.IO.Path.Combine(folderPath, "folder.ico");
+
+                // Check if folder.icon exists
+                if (File.Exists(iconFilePath))
+                {
+                    WriteLog("Updating folder icon for " + folderPath);
+                    FolderIconUpdater.ChangeFolderIcon(folderPath, iconFilePath);
+                }
+            }
+
+            WriteLog("Done!");
+            MessageBox.Show("Done!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+    }
+}
